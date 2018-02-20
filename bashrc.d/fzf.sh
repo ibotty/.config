0101389a090ae37def758482c1585d3008ec0acb
Tobias Florek <tob@butter.sh>
2018-02-20 10:36:56 +0100

    add fzf configuration

---
+++ bashrc.d/fzf.sh
@@ -0,0 +1,5 @@
+alias fzf=fzf-tmux
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,vendor}/*" 2> /dev/null'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="rg --hidden --files -g '!{.git,node_modules,vendor}/*' --null 2> /dev/null | xargs -0 dirname | awk '!h[\$0]++'"
+bind -x '"\C-p": nvim $(fzf);'
