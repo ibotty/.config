9fc3019f78c83619e8cb91dac9329f4818c750bb
Tobias Florek <tob@butter.sh>
2018-02-20 10:39:13 +0100

    use fzf key bindings

--- bashrc.d/fzf.sh
+++ bashrc.d/fzf.sh
@@ -1,3 +1,6 @@
+[ -f /usr/share/fzf/shell/key-bindings.bash ] && \
+    source /usr/share/fzf/shell/key-bindings.bash
+
 alias fzf=fzf-tmux
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,vendor}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
