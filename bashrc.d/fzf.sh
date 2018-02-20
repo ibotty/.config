147c1e6c8da46db0ed06e373f43bf2f5b47d5103
Tobias Florek <tob@butter.sh>
2018-02-20 10:40:40 +0100

    only use fzf when it is installed

--- bashrc.d/fzf.sh
+++ bashrc.d/fzf.sh
@@ -1,8 +1,11 @@
 [ -f /usr/share/fzf/shell/key-bindings.bash ] && \
     source /usr/share/fzf/shell/key-bindings.bash
 
-alias fzf=fzf-tmux
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,vendor}/*" 2> /dev/null'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="rg --hidden --files -g '!{.git,node_modules,vendor}/*' --null 2> /dev/null | xargs -0 dirname | awk '!h[\$0]++'"
-bind -x '"\C-p": nvim $(fzf);'
+if hash fzf 2> /dev/null; then
+
+    alias fzf=fzf-tmux
+    export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,vendor}/*" 2> /dev/null'
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+    export FZF_ALT_C_COMMAND="rg --hidden --files -g '!{.git,node_modules,vendor}/*' --null 2> /dev/null | xargs -0 dirname | awk '!h[\$0]++'"
+    bind -x '"\C-p": nvim $(fzf);'
+fi
