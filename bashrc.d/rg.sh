803e69af955f1930ac42f48bf85b5dfb5d67af27
Tobias Florek <tob@butter.sh>
2018-02-20 10:33:13 +0100

    merge ripgrep bash snippets

--- bashrc.d/rg.sh
+++
@@ -1,7 +0,0 @@
-rg() {
-    if [ -t 1 ]; then
-        command rg -p "$@" | less -RFX
-    else
-        command rg "$@"
-    fi
-}
--- bashrc.d/ripgrep.sh
+++ bashrc.d/ripgrep.sh
@@ -1,1 +1,9 @@
+rg() {
+    if [ -t 1 ]; then
+        command rg -p "$@" | less -RFX
+    else
+        command rg "$@"
+    fi
+}
+
 export RIPGREP_CONFIG_PATH="$HOME/.config/rg.conf"
